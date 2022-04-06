c7ed2a12c1c649713be47014e3cad5ef8a61ab94
vanshsaini15 <92376023+vanshsaini15@users.noreply.github.com>
2022-04-06 13:25:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -100,69 +100,25 @@ function App() {
     setPlayer("O");
   };
 
-
-  return (
+return (
     <div className="App">
       <div className="board">
         <div className="row">
-          <Square
-            val={board[0]}
-            chooseSquare={() => {
-              chooseSquare(0);
-            }}
-          />
-          <Square
-            val={board[1]}
-            chooseSquare={() => {
-              chooseSquare(1);
-            }}
-          />
-          <Square
-            val={board[2]}
-            chooseSquare={() => {
-              chooseSquare(2);
-            }}
-          />
+          <Square val={board[0]} rendSquare={() => { rendSquare(0); }} />
+          <Square val={board[1]} rendSquare={() => { rendSquare(1); }} />
+          <Square val={board[2]} rendSquare={() => { rendSquare(2); }} />
         </div>
+
         <div className="row">
-          <Square
-            val={board[3]}
-            chooseSquare={() => {
-              chooseSquare(3);
-            }}
-          />
-          <Square
-            val={board[4]}
-            chooseSquare={() => {
-              chooseSquare(4);
-            }}
-          />
-          <Square
-            val={board[5]}
-            chooseSquare={() => {
-              chooseSquare(5);
-            }}
-          />
+          <Square val={board[3]} rendSquare={() => { rendSquare(3); }} />
+          <Square val={board[4]} rendSquare={() => { rendSquare(4); }} />
+          <Square val={board[5]} rendSquare={() => { rendSquare(5); }} />
         </div>
+
         <div className="row">
-          <Square
-            val={board[6]}
-            chooseSquare={() => {
-              chooseSquare(6);
-            }}
-          />
-          <Square
-            val={board[7]}
-            chooseSquare={() => {
-              chooseSquare(7);
-            }}
-          />
-          <Square
-            val={board[8]}
-            chooseSquare={() => {
-              chooseSquare(8);
-            }}
-          />
+          <Square val={board[6]} rendSquare={() => { rendSquare(6); }} />
+          <Square val={board[7]} rendSquare={() => { rendSquare(7); }} />
+          <Square val={board[8]} rendSquare={() => { rendSquare(8); }} />
         </div>
       </div>
     </div>
